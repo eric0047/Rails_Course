dc81bfb811e0ca1957827064b75c09f9218afd92
Eric <eric00471@gmail.com>
2023-03-21 16:57:30 +0800

    add resumes form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/resumes", to: "resumes#index"
+  get "/resumes/new", to: "resumes#new"
   get "/about", to: "clubs#about"
   get "/contact", to: "clubs#contact"
 end
